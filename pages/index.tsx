347ed470a3734ff67f8adccecc07d8a40fe55d46
colombatapia <48482556+colombatapia@users.noreply.github.com>
2022-05-02 18:48:48 -0400

    Update pages/index.tsx
    Co-authored-by: Daniel Leal <37158127+daleal@users.noreply.github.com>

--- pages/index.tsx
+++ pages/index.tsx
@@ -12,7 +12,6 @@ const Home: NextPage = () => {
     <div className="relative bg-white overflow-hidden">
       <div className="max-w-7xl mx-auto">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-        
           <Head>
             <svg>
               <defs>
