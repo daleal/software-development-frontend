bab56a1860b7555959a8cb844e0fde02ed58417c
Daniel Leal <dlleal@uc.cl>
2022-05-28 19:03:00 -0400

    Stop endless request loop

--- api/setup.ts
+++ api/setup.ts
@@ -1,9 +1,10 @@
 import client from './client'
 import { Nullable } from '@/types/common'
 
-export const setupAPIAuthInterceptors = (getToken: () => Promise<Nullable<string>>) => {
+export const setupAPIAuthInterceptors = (getToken: (options?: { refresh?: boolean }) => Promise<Nullable<string>>) => {
   client.interceptors.request.use(async (config) => {
-    const token = await getToken()
+    const refresh = config.url === '/auth/jwt/refresh/' ? false : true
+    const token = await getToken({ refresh })
 
     if (token) {
       config.headers = {
