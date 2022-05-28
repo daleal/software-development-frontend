b47a9599b8d196f45dc26ab39324f6d0c31c8161
Daniel Leal <dlleal@uc.cl>
2022-05-28 19:04:36 -0400

    Add a guard component

---
+++ components/guards/RequireLoggedIn.tsx
@@ -0,0 +1,36 @@
+import { useEffect, useState } from 'react'
+import type { AppProps } from 'next/app'
+import { useRouter } from 'next/router'
+import { useSession } from '@/hooks/session'
+
+const AUTH_PATHS = ['/login', '/signup']
+const NO_LOGIN_REQUIRED_PATHS = ['/']
+
+export const RequireLoggedIn = ({ Component, pageProps }: AppProps) => {
+  const router = useRouter()
+  const { getToken } = useSession()
+
+  const [loading, setLoading] = useState(true)
+
+  useEffect(() => {
+    const checkUserLoggedIn = async () => {
+      if (![...AUTH_PATHS, ...NO_LOGIN_REQUIRED_PATHS].includes(router.pathname)) {
+        await getToken()
+      }
+      if (AUTH_PATHS.includes(router.pathname)) {
+        try {
+          await getToken({ redirect: false })
+          await router.push('/')
+        } catch { }
+      }
+      setLoading(false)
+    }
+    checkUserLoggedIn()
+  }, []); // eslint-disable-line react-hooks/exhaustive-deps
+
+  if (loading) {
+    return <div>Loading...</div>
+  }
+
+  return <Component {...pageProps} />
+}
