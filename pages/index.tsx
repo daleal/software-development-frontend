9cbe2cf4feb79cab1fe03a8da9fce772d37a9540
KnowYourselves <rsalvarez@uc.cl>
2022-05-30 01:39:33 -0400

    feat: add index padding

--- pages/index.tsx
+++ pages/index.tsx
@@ -5,11 +5,11 @@ import Image from "next/image";
 const Home: NextPage = () => {
   return (
     <>
-      <div className="max-w-7xl mx-auto">
+      <div className="px-10 mx-auto max-w-7xl">
         <div className="relative z-10 pb-8 bg-white sm:pb-16 md:pb-20 lg:max-w-2xl lg:w-full lg:pb-28 xl:pb-32">
-          <main className="mt-10 mx-auto max-w-7xl px-4 sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
+          <main className="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
-              <h1 className="text-4xl tracking-tight font-extrabold text-gray-900 sm:text-5xl md:text-6xl">
+              <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
                 <span className="block text-blue-800 xl">Rentool</span>{" "}
                 <span className="block text-yellow-400 xl">
                   El Uber de las herramientas
@@ -26,7 +26,7 @@ const Home: NextPage = () => {
       </div>
       <div className="lg:absolute lg:inset-y-0 lg:right-0">
         <Image
-          className="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full"
+          className="object-cover w-full h-56 sm:h-72 md:h-96 lg:w-full lg:h-full"
           src="/tools.jpeg"
           alt="Vercel Logo"
           width={800}
