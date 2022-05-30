ef391135a3d1c4b68130357a4e8f0f9ebb6a36d4
Daniel Leal <dlleal@uc.cl>
2022-05-30 02:50:30 -0400

    Add the store provider to the app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 import { useSession } from '@/hooks/session'
-import '@/styles/globals.css'
+import { store } from '@/store'
 import { setupAPIAuthInterceptors } from '@/api/setup'
+import '@/styles/globals.css'
 
 function MyApp({ Component, pageProps }: AppProps) {
   const { getToken } = useSession()
 
   setupAPIAuthInterceptors(getToken)
 
-  return <Component {...pageProps} />
+  return <>
+    <Provider store={store}>
+      <Component {...pageProps} />
+    </Provider>
+  </>
 }
 
 export default MyApp
