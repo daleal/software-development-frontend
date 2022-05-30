dd58933bea0f814aecf381a4b1ec02218fac62db
javieraochg <javiera.ochoa@pinflag.cl>
2022-05-29 22:25:20 -0400

    fix style code

--- api/setup.ts
+++ api/setup.ts
@@ -1,22 +1,19 @@
-import client from "./client";
-import { Nullable } from "@/types/common";
+import client from './client'
+import { Nullable } from '@/types/common'
+import type { GetTokenOptions } from '@/hooks/session'
 
-export const setupAPIAuthInterceptors = (
-  getToken: () => Promise<Nullable<string>>
-) => {
+export const setupAPIAuthInterceptors = (getToken: () => Promise<Nullable<string>>) => {
   client.interceptors.request.use(async (config) => {
-    if (
-      !["/auth/jwt/refresh/", "/auth/jwt/verify/"].includes(config.url ?? "")
-    ) {
-      const token = await getToken();
+    if (!['/auth/jwt/refresh/', '/auth/jwt/verify/'].includes(config.url ?? '')) {
+      const token = await getToken()
 
       if (token) {
         config.headers = {
           ...config.headers,
           Authorization: `JWT ${token}`,
-        };
+        }
       }
     }
-    return config;
-  });
-};
+    return config
+  })
+}
