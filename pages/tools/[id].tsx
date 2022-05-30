5add5f6fee25f430a7c76001af36d046936c3d61
javieraochoa <jiochoa@uc.cl>
2022-05-30 17:52:29 -0400

    redirect and style

--- pages/tools/[id].tsx
+++ pages/tools/[id].tsx
@@ -7,6 +7,7 @@ import type { NextPage } from 'next'
 import { loadToolListing } from '@/store/modules/toolListings'
 import type { Nullable } from '@/types/common'
 import type { ToolListing } from '@/types/entities/toolListing'
+import Image from 'next/image'
 
 
 const ToolListingDetail: NextPage = () => {
@@ -28,17 +29,31 @@ const ToolListingDetail: NextPage = () => {
     <div className="bg-white">
       <div className="pt-6 pb-16 sm:pb-24">
         <div className="max-w-2xl px-4 mx-auto mt-8 sm:px-6 lg:max-w-7xl lg:px-8">
-          <div className="lg:grid lg:grid-cols-12 lg:auto-rows-min lg:gap-x-8">
-            {!toolListing
+          <div className="lg:grid lg:grid-cols-4 lg:auto-rows-min lg:gap-x-8">
+            {loading
               ? <p>Loading</p>
-              : (
-                <div className="lg:col-start-8 lg:col-span-5">
+              : 
+              toolListing ?
+              ( 
+                <>
+                <div className="lg:col-start-1 lg:col-span-2">
                   <div className="flex justify-between">
-                    <h1 className="text-xl font-medium text-gray-900">{toolListing.name}</h1>
-                    <p className="text-xl font-medium text-gray-900">{toolListing.price}</p>
+                    <Image alt={toolListing.name}
+                      className="rounded-t-lg"
+                      width={450}
+                      height={450}
+                      src={toolListing.image} />
+                      <div className="lg:col-start-3 lg:col-span-2 mt-16">
+                        <h1 className="font-bold text-4xl text-blue-800">{toolListing.name}</h1>
+                        <p className="text-sm text-gray-500">{toolListing.price}</p>
+                      </div>
                   </div>
                 </div>
+                
+                </>
               )
+              :
+              <p>No se ha encontrado la herramienta.</p>
             }
           </div>
         </div>
