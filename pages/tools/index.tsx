c8722b91bd58ac3917765f3d2f194b8a593ce330
javieraochoa <jiochoa@uc.cl>
2022-05-30 14:15:04 -0400

    remove heroku domain

--- pages/tools/index.tsx
+++ pages/tools/index.tsx
@@ -2,7 +2,6 @@ import { useEffect, useState } from 'react'
 import type { NextPage } from 'next'
 import { loadToolListings } from '@/store/modules/toolListings'
 import { useSelector, useDispatch } from '@/store'
-import type { Nullable } from '@/types/common'
 import type { ToolListing } from '@/types/entities/toolListing'
 import Image from 'next/image'
 
@@ -24,7 +23,7 @@ const ListTools: NextPage = () => {
     { loading? 
     <>loading...</>
     : 
-    <div className=" m-10 bg-white shadow overflow-hidden sm:rounded-md">
+    <div className=" m-24 bg-white shadow overflow-hidden sm:rounded-md">
       <div className="grid grid-cols-3 gap-8 m-10">
         {toolListings.map((tool: ToolListing) =>
         <div key={tool.id} className="max-w-sm rounded overflow-hidden shadow-lg">
@@ -37,7 +36,7 @@ const ListTools: NextPage = () => {
           </div>
           <div className="p-5">
             <div className="font-bold text-xl mb-2">{ tool.name }</div> 
-            <p className="text-gray-700 text-base">${ tool.price }</p> 
+            <p className="text-sm text-gray-700">Precio: ${ tool.price }</p> 
           </div>
         </div> 
         )}
