43fc745a466aadc50e3366f346882a37ba925cb1
KnowYourselves <rsalvarez@uc.cl>
2022-05-30 02:42:54 -0400

    fix: add price to form

--- pages/new-listing.tsx
+++ pages/new-listing.tsx
@@ -48,6 +48,23 @@ const NewListing: NextPage = () => {
             </div>
             <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
               <label
+                htmlFor="price"
+                className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
+              >
+                Precio
+              </label>
+              <div className="mt-1 sm:mt-0 sm:col-span-2">
+                <input
+                  type="number"
+                  name="price"
+                  id="price"
+                  autoComplete="given-name"
+                  className="block w-full max-w-lg border-gray-300 rounded-md shadow-sm focus:ring-indigo-500 focus:border-indigo-500 sm:max-w-xs sm:text-sm"
+                />
+              </div>
+            </div>
+            <div className="sm:grid sm:grid-cols-3 sm:gap-4 sm:items-start sm:border-t sm:border-gray-200 sm:pt-5">
+              <label
                 htmlFor="about"
                 className="block text-sm font-medium text-gray-700 sm:mt-px sm:pt-2"
               >
