8192c7e21981da186e3b67e5504e8159d6a1bc95
Daniel Leal <37158127+daleal@users.noreply.github.com>
2022-05-30 12:10:07 -0400

    Add sample store (#12)

---
+++ api/modules/toolListings.ts
@@ -0,0 +1,21 @@
+import client from '@/api/client'
+import type { ToolListing } from '@/types/entities/toolListing'
+
+export const list = async (): Promise<Array<ToolListing>> => {
+  const response = await client.get('/api/tool-listings/')
+  return response.data
+}
+
+export const get = async (id: number): Promise<ToolListing> => {
+  const response = await client.get(`/api/tool-listings/${id}/`)
+  return response.data
+}
+
+export const create = async (
+  name: string,
+  description: string,
+  price: number,
+): Promise<ToolListing> => {
+  const response = await client.post('/api/tool-listings/', { name, description, price })
+  return response.data
+}
--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,21 @@
 import type { AppProps } from 'next/app'
+import { Provider } from 'react-redux'
 import { useSession } from '@/hooks/session'
-import '@/styles/globals.css'
+import { store } from '@/store'
 import { setupAPIAuthInterceptors } from '@/api/setup'
 import { RequireLoggedIn } from '@/components/guards/RequireLoggedIn'
+import '@/styles/globals.css'
 
 function MyApp(appProps: AppProps) {
   const { getToken } = useSession()
 
   setupAPIAuthInterceptors(getToken)
 
-  return <RequireLoggedIn {...appProps} />
+  return <>
+    <Provider store={store}>
+      <RequireLoggedIn {...appProps} />
+    </Provider>
+  </>
 }
 
 export default MyApp
