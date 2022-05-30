ed3cc0bf43167f6887c2e7082eb66b1f4f1146da
Nicolas <naquiroz@uc.cl>
2022-05-30 00:52:39 -0400

    feat: remove button

--- pages/new-listing.tsx
+++ pages/new-listing.tsx
@@ -109,16 +109,10 @@ const Home: NextPage = () => {
       <div className="pt-5">
         <div className="flex justify-end">
           <button
-            type="button"
-            className="bg-white py-2 px-4 border border-gray-300 rounded-md shadow-sm text-sm font-medium text-gray-700 hover:bg-gray-50 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
-          >
-            Cancel
-          </button>
-          <button
             type="submit"
             className="ml-3 inline-flex justify-center py-2 px-4 border border-transparent shadow-sm text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500"
           >
-            Save
+            Publicar herramienta 🚀
           </button>
         </div>
       </div>
