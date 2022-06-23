5c7167ea29a23b9e3b87ac6d0806c8ba1cd4201f
javieraochoa <jiochoa@uc.cl>
2022-06-23 16:11:43 -0400

    fix interceptors

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,15 +2,20 @@ import type { AppProps } from 'next/app'
 import { Provider } from 'react-redux'
 import { useSession } from '@/hooks/session'
 import { store } from '@/store'
-import { setupAPIAuthInterceptors } from '@/api/setup'
+import { ejectInterceptors, setupAPIAuthInterceptors } from '@/api/setup'
 import WithNavbar from '@/components/layouts/WithNavbar';
 import { RequireLoggedIn } from '@/components/guards/RequireLoggedIn'
 import '@/styles/globals.css'
+import { useEffect } from 'react'
 
 function MyApp(appProps: AppProps) {
   const { getToken, logout } = useSession()
 
-  setupAPIAuthInterceptors(getToken, logout);
+  useEffect(()=> {
+    const [reqInterceptor, resInterceptor] = setupAPIAuthInterceptors(getToken, logout)
+    return () => ejectInterceptors(reqInterceptor, resInterceptor)
+  }), []
+ 
 
   return <>
     <Provider store={store}>
