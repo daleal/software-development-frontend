c302cea5e0a07f5c4d70ae040d0a3f5cdc487e5a
Raul Alvarez <raul.alvarez@magnet.cl>
2022-06-24 00:17:42 -0400

    fix: remove lorem ipsum from index

--- pages/index.tsx
+++ pages/index.tsx
@@ -15,11 +15,6 @@ const Home: NextPage = () => {
                   El Uber de las herramientas
                 </span>
               </h1>
-              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Anim aute id magna aliqua ad ad non deserunt sunt. Qui irure qui
-                lorem cupidatat commodo. Elit sunt amet fugiat veniam occaecat
-                fugiat aliqua.
-              </p>
             </div>
           </main>
         </div>
