134c09d3bf7e0605643fd788c94fcf87e874b526
Nicolas <naquiroz@uc.cl>
2022-06-24 12:50:18 -0400

    feat: add want to rent

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,6 @@
-import type { NextPage } from 'next'
-import Image from 'next/image'
+import type { NextPage } from "next";
+import Image from "next/image";
+import Link from "next/link";
 
 // Reference: https://tailwindui.com/components/marketing/sections/heroes
 const Home: NextPage = () => {
@@ -10,16 +11,23 @@ const Home: NextPage = () => {
           <main className="px-4 mx-auto mt-10 max-w-7xl sm:mt-12 sm:px-6 md:mt-16 lg:mt-20 lg:px-8 xl:mt-28">
             <div className="sm:text-center lg:text-left">
               <h1 className="text-4xl font-extrabold tracking-tight text-gray-900 sm:text-5xl md:text-6xl">
-                <span className="block text-blue-800 xl">Rentool</span>{' '}
+                <span className="block text-blue-800 xl">Rentool</span>{" "}
                 <span className="block text-yellow-400 xl">
                   El Uber de las herramientas
                 </span>
               </h1>
-               <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
-                Arrienda herramientas a a bajo costos. Quita el polvo a esas herramientas que guardas, y 
-                ayuda a otros a lograr sus proyectos. Rentool es una plataforma que te permite realizar arriendos con un solo click.
+              <p className="mt-3 text-base text-gray-500 sm:mt-5 sm:text-lg sm:max-w-xl sm:mx-auto md:mt-5 md:text-xl lg:mx-0">
+                Arrienda herramientas a a bajo costos. Quita el polvo a esas
+                herramientas que guardas, y ayuda a otros a lograr sus
+                proyectos. Rentool es una plataforma que te permite realizar
+                arriendos con un solo click.
               </p>
             </div>
+            <Link href="/tools">
+              <a className="mt-2 max-w-xs flex justify-center py-2 px-4 border rounded-md shadow-sm text-sm font-medium text-blue-700 border-blue-700 hover:bg-blue-700 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-blue-800 cursor-pointer hover:scale-105 transition duration-150 ease-in-out">
+                Quiero arrendar
+              </a>
+            </Link>
           </main>
         </div>
       </div>
@@ -33,7 +41,7 @@ const Home: NextPage = () => {
         />
       </div>
     </>
-  )
-}
+  );
+};
 
-export default Home
+export default Home;
